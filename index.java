87adb65a3d78689186f82063a6e3b58cab902bde
pratik <pratik7887838793@gmail.com>
2024-03-01 09:52:20 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,13 @@
+/**
+ * index
+ */
+public class index {
+
+    public static void main(String[]args){
+        System.out.println("Hello basic Jencking ruunig");
+        System.out.println("Hello i am pratik");
+        
+
+    }  
+}
+   
