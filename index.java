d7bcb30d8eb040ae48d927df56db3a0a6773b544
pratik <pratik7887838793@gmail.com>
2024-03-01 10:28:35 +0530

    adding file

--- index.java
+++ index.java
@@ -6,6 +6,7 @@ public class index {
     public static void main(String[]args){
         System.out.println("Hello basic Jencking ruunig");
         System.out.println("Hello i am pratik");
+        System.out.println("one more adding with git put");
         
 
     }  
